76f9d18f0dd84065bebdc0932da9a32014b0e5ca
KarthikeyaKollu <karthikeyakollu8@gmail.com>
2025-10-01 21:23:14 +0530

    Initial commit

--- src/components/chanhdai-mark.tsx
+++ src/components/chanhdai-mark.tsx
@@ -1,7 +1,6 @@
 export function ChanhDaiMark(props: React.ComponentProps<"svg">) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 112 64" {...props}>
-    </svg>
+    <div className="text-3xl">💻</div>
   );
 }
 
